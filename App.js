560c54df7a7fa50e53fd53fabf59c71b3fd10499
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-10 12:47:20 -0600

    finishin the pokecard

--- App.js
+++ App.js
@@ -1,10 +1,53 @@
-import { StyleSheet, SafeAreaView, Platform } from 'react-native';
+import { StyleSheet, SafeAreaView, Platform, ScrollView } from 'react-native';
 import PokemonCard from './components/PokemonCard';
 
 export default function App() {
+  const pokemonData = [
+    {
+      name: "Charmander",
+      image: require("./assets/charmander.png"),
+      type: "Fire",
+      hp: 39,
+      moves: ["Scratch", "Ember", "Growl", "Leer"],
+      weaknesses: ["Water", "Rock"],
+      decorators: { borderColor: "#FF5733", emoji: "🔥" }
+    },
+    {
+      name: "Squirtle",
+      image: require("./assets/squirtle.png"),
+      type: "Water",
+      hp: 44,
+      moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+      weaknesses: ["Electric", "Grass"],
+      decorators: { borderColor: "#6493EA", emoji: "💧" }
+    },
+    {
+      name: "Bulbasaur",
+      image: require("./assets/bulbasaur.png"),
+      type: "Grass",
+      hp: 45,
+      moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+      weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+      decorators: { borderColor: "#66CC66", emoji: "🌿" }
+    },
+    {
+      name: "Pikachu",
+      image: require("./assets/pikachu.png"),
+      type: "Electric",
+      hp: 35,
+      moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+      weaknesses: ["Ground"],
+      decorators: { borderColor: "#FFD700", emoji: "⚡️" }
+    }
+  ];
+
   return (
     <SafeAreaView style={styles.container}>
-      <PokemonCard />
+      <ScrollView>
+        {pokemonData.map((pokemon, i) => (
+          <PokemonCard key={i} pokemon={pokemon} />
+        ))}
+      </ScrollView>
     </SafeAreaView>
   );
 }
--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,10 +1,32 @@
-import { View, Text, StyleSheet, Platform } from 'react-native'
+import { View, Text, StyleSheet, Platform, Image } from 'react-native'
 import React from 'react'
 
-export default function PokemonCard() {
+export default function PokemonCard({ pokemon }) {
     return (
-        <View style={styles.card}>
-            <Text>card</Text>
+        <View style={[styles.card, { borderColor: pokemon.decorators.borderColor }, { shadowColor: pokemon.decorators.borderColor }]}>
+
+            <View style={styles.nameContainer} >
+                <Text style={styles.name} >{pokemon.name}</Text>
+                <Text style={styles.hp} >♥️{pokemon.hp}</Text>
+            </View>
+
+            <Image style={styles.image} resizeMode='contain' source={pokemon.image} accessibilityLabel={`${pokemon.name} pokemon`} />
+
+            <View style={styles.typeContainer} >
+                <View style={[styles.badge, {borderColor: pokemon.decorators.borderColor}]} >
+                    <Text style={styles.typeEmoji} >{pokemon.decorators.emoji}</Text>
+                    <Text style={styles.typeText} >{pokemon.type}</Text>
+                </View>
+            </View>
+
+            <View style={styles.listContainer} >
+                <Text style={styles.listText} >Moves: {pokemon.moves.join(", ")}</Text>
+            </View>
+
+            <View style={styles.listContainer} >
+                <Text style={styles.listText} >weaknesses: {pokemon.weaknesses.join(", ")}</Text>
+            </View>
+
         </View>
     )
 }
@@ -13,19 +35,63 @@ const styles = StyleSheet.create({
     card: {
         backgroundColor: "white",
         borderRadius: 16,
-        borderWidth: 2,
+        borderWidth: 3,
         padding: 16,
         margin: 16,
         ...Platform.select({
             ios: {
-                shadowOffset: { width: 2, height: 2 },
-                shadowColor: "#333",
-                shadowOpacity: 0.3,
-                shadowRadius: 4,
+                shadowOffset: { width: 4, height: 3 },
+                shadowOpacity: 1,
+                shadowRadius: 7,
             },
             android: {
                 elevation: 5,
             }
         })
+    }
+    ,
+    nameContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 32
+    },
+    name: {
+        fontSize: 30,
+        fontWeight: "bold"
+    },
+    hp: {
+        fontSize: 22
+    },
+    image: {
+        width: "100%",
+        height: 200,
+        marginBottom: 16
+    },
+    typeContainer: {
+        alignItems: "center",
+        marginBottom: 40,
+    },
+    badge: {
+        flexDirection: "row",
+        alignItems: "center",
+        paddingVertical: 6,
+        paddingHorizontal: 12,
+        borderRadius: 20,
+        borderWidth: 4,
+    },
+    typeEmoji: {
+        fontSize: 30,
+        marginRight: 12
+    },
+    typeText: {
+        fontSize: 22,
+        fontWeight: "bold"
+    },
+    listContainer: {
+       marginBottom: 16
+    },
+    listText: {
+        fontSize: 22,
+        fontWeight: "bold"
     }
 })
