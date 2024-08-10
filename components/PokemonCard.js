1694835cb0fed599c3107605991d341b9bfe8141
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-09 20:39:31 -0600

    styling the poke card

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Platform } from 'react-native'
 import React from 'react'
 
 export default function PokemonCard() {
-  return (
-    <View>
-      <Text>PokemonCard</Text>
-    </View>
-  )
+    return (
+        <View style={styles.card}>
+            <Text>card</Text>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    card: {
+        backgroundColor: "white",
+        borderRadius: 16,
+        borderWidth: 2,
+        padding: 16,
+        margin: 16,
+        ...Platform.select({
+            ios: {
+                shadowOffset: { width: 2, height: 2 },
+                shadowColor: "#333",
+                shadowOpacity: 0.3,
+                shadowRadius: 4,
+            },
+            android: {
+                elevation: 5,
+            }
+        })
+    }
+})
